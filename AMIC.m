f337ef4ab10002e28e342f7381cb0d63ee8f9321
salva-olacua <salvaolacua@gmail.com>
2017-10-30 22:16:51 -0300

    mejora de interfaz y creo aproximacion por recta junto con el grafico

--- AMIC.m
+++ AMIC.m
@@ -4,13 +4,26 @@ pkg load signal;
 clear;
 
 
-function cargarDatos
-  %cargar los datos!!!
-  display('formato de carga x=[ X0,X1,X2,...,Xn], y=[f(X0),f(X1),f(X2),...,f(Xn)]');
-  x=input('x= ');
-  y=input('y= ');
-  display('datos cargados'); 
+function [x,y,cant_decimales]=cargarDatos
+  %errordlg("hgj"); utilizar para mensajes de error
+  msgbox({'Formato de carga: ' 'x=[ X0,X1,X2,...,Xn]'  'y=[f(X0),f(X1),f(X2),...,f(Xn)]'});
+  x_string=inputdlg('x= ','AMIC');
+  x=str2num(x_string{1});
+  y_string=inputdlg('y= ','AMIC');
+  y=str2num(y_string{1});
+  msgbox('Ingrese la cantidad de decimales que desea');
+  dec_string=inputdlg('cantidad de decimales','AMIC');
+  cant_decimales=str2num(dec_string{1});
+  msgbox('datos cargados'); 
+  %coeficientes=polyfit(x,y,1);
+  %puntos_a_evaluar = linspace(1,4,20);
+  %recta_de_aproximacion=polyval(coeficientes,puntos_a_evaluar);
+  %plot(x, y,'r*');
+  %hold on
+  %plot(puntos_a_evaluar,recta_de_aproximacion,'m-.');
+  %hold off
 endfunction
+
 
 
 function compararAproximaciones
@@ -19,14 +32,43 @@ function compararAproximaciones
 endfunction
 
 
+
+
+
+
+function [a,b]=aproximacionPorRecta(x,y)
+  sumatoria_x=sum(x);
+  sumatoria_y=sum(y);
+  
+  tamanio_x=length(x);
+  
+  sumatoria_x_al_cuadrado=sum(x.^2);
+  
+  sumatoria_x_y = sum(x.*y);
+  
+  matriz_coeficientes= [sumatoria_x_al_cuadrado,sumatoria_x ; sumatoria_x,tamanio_x];
+  matriz_aumentada= [sumatoria_x_y ; sumatoria_y];
+  matriz_resultante= inv(matriz_coeficientes) * matriz_aumentada;
+  a=matriz_resultante(1,1);
+  b=matriz_resultante(2,1);
+ 
+  
+endfunction
+
+
+
+function mostrarFuncionAproximante(a,b,funcionAproximante)
+  msgbox({funcionAproximante strcat("a= ",num2str(a)) strcat("b= ",num2str(b)) });
+endfunction
+
+
 function menuDeOpciones
-  %disp("Ingresar datos para la aproximacion")
  while(true)
   opcion = menu ("menu de opciones", "Cargar Datos", "Comparar Aproximaciones","Finalizar");
     switch (opcion)
       case 1
-        cargarDatos();
-        menuDeAproximaciones();
+        [x,y,cant_decimales]=cargarDatos();
+        menuDeAproximaciones(x,y,cant_decimales);
       case 2
         compararAproximaciones();
       case 3
@@ -37,9 +79,40 @@ endfunction
 
 
 
+function visualizarGrafica(x,y,a,b)
+  plot(x, y,'r*');
+  puntos_a_evaluar = linspace(1,4,20);
+  recta_de_aproximacion=polyval([a b],puntos_a_evaluar);
+  hold on
+  plot(puntos_a_evaluar,recta_de_aproximacion,'m-.');
+  hold off
+endfunction
 
-function menuDeAproximaciones
-  
+
+
+function menuDeOpcionesPorAproximacion(x,y,a,b,grafica)
+  while(true)
+  opcion = menu ("menu de interaccion por aproximacion elegida",
+                 "Mostrar la funcion aproximante",
+                 "Obtener el detalle del calculo",
+                 "Visualizar grafica",
+                 "Volver al menu de aproximacion");
+    switch (opcion)
+      case 1
+        mostrarFuncionAproximante(a,b,grafica);
+      case 2
+        detalleDelCalculo();
+      case 3
+        visualizarGrafica(x,y,a,b);
+      case 4
+        break;
+    endswitch
+ endwhile
+endfunction
+
+
+
+function menuDeAproximaciones(x,y,cant_decimales)  
 while(true)
   opcion = menu ("Aproximar mediante: ",
     "1. Recta de mínimos cuadrados: y = ax+b.",
@@ -50,15 +123,17 @@ while(true)
     "6. Volver al menu de opciones .");
   switch (opcion)
     case 1
-     opcion1()
+     [a,b]=aproximacionPorRecta(x,y);
+     recta="y=mx+b";
+     menuDeOpcionesPorAproximacion(x,y,a,b,recta);
     case 2
-     opcion2()
+     opcion2();
     case 3
-     opcion3()
+     opcion3();
     case 4
-     opcion4()
+     opcion4();
     case 5
-     opcion5()
+     opcion5();
     case 6
      break;
   endswitch
